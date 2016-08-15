479147eface4703dd865ab25e6930f32d45899d8
Chaos Eternal <chaos@shlug.org>
2016-08-15 17:35:14 +0800

    explain analyze added

--- sql-tmux-runner.el
+++ sql-tmux-runner.el
@@ -34,26 +34,26 @@
    (interactive)
    (sql-send-region (point-min) (point-max)))
 
-
-(defun sql-explain-region (start end)
-   (interactive "r")
+(defun sql-explain-region (start end &optional analyze)
+  (interactive "r\nsExplain(e) or Explain Analyze(a): ")
    (let ((header-end (sql-prepare-with-header)))
      (shell-command-on-region 
       (if (> header-end start) header-end start) end
-      "send-sql.sh explain")))
+      (if (eq 0 (string-match analyze "analyze")) "send-sql.sh analyze"
+	"send-sql.sh explain"))))
 
 
 (define-key sql-mode-map (kbd "C-c M-r")
    'sql-explain-region)
 
 
-(defun sql-explain-paragraph ()
-   (interactive)
+(defun sql-explain-paragraph (&optional analyze)
+   (interactive "sExplain(e) or Explain Analyze(a): " )
    (let ((start (save-excursion
 		  (backward-paragraph)
 		  (point)))
 	 (end (point)))
-     (sql-explain-region start end)))
+     (sql-explain-region start end analyze)))
 
 (define-key sql-mode-map (kbd "C-c M-c")
    'sql-explain-paragraph)
